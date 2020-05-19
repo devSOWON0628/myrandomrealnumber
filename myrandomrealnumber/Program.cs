d15f02d6174a1c82d95d2a5137b90b2e1f32d167
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-19 16:22:48 +0900

    초기 커밋.

---
+++ myrandomrealnumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myrandomrealnumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int correct = random.Next(0, 500);
+            int res;
+            
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요 :");
+                res = int.Parse(Console.ReadLine());
+                if (res > correct)
+                {
+                    Console.WriteLine(res + "보다 작은 숫자입니다.");
+                    Console.WriteLine("");
+                }
+                else if (res < correct)
+                {
+                    Console.WriteLine(res + "보다 큰 숫자입니다");
+                    Console.WriteLine("");
+
+                }
+
+                else if (res == correct)
+                {
+                    Console.WriteLine("정답입니다");
+                    Console.WriteLine("");
+
+                    break;
+                }
+            }
+            
+        }
+    }
+}
